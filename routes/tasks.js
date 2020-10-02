24e23708b48af93dc8beee2482e0da4992a80ebb
Aman Desai <amandesai01@gmail.com>
2020-10-02 12:10:45 +0530

    [FIX] Removing Unused Import

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const verifyToken = require('./../middlewares/verifyToken');
 const Task = require('./../models/Task');
-const User = require('../models/User');
 const router = express.Router();
 
 router.get('/', verifyToken, (request, response) => {
